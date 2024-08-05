2e425ed99433cd6a927e82f1b0fdd5431cbf3fe7
Dineshgayke12 <103435929+Dineshgayke12@users.noreply.github.com>
2024-08-05 05:52:17 -0700

    Add files via upload

---
+++ funInterface/src/com/java/funInterface/Double.java
@@ -0,0 +1,5 @@
+package com.java.funInterface;
+
+public interface Double {
+
+}
---
+++ funInterface/src/com/java/funInterface/MyMain.java
@@ -0,0 +1,12 @@
+package com.java.funInterface;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		
+		FuncInt fi = new Function();
+		fi.myprint(Hello Everyone);
+
+	}
+
+}
